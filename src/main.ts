2b84019550edd83fad88c0f93f5c0b9fb8ffc56c
thienvd <thienvd@apero.vn>
2023-08-02 16:08:02 +0700

    swager

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { APP_CONFIG } from './configs/app.config';
 import nestConfig from './configs/nest.config';
 import bootstrapConfig from './configs/bootstrap.config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const logger: Logger = new Logger('Main');
 const port = APP_CONFIG.ENV.APP.PORT || 9999;
@@ -13,6 +14,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   nestConfig(app);
   bootstrapConfig();
+
+  const config = new DocumentBuilder()
+    .setTitle('TRUE MATE AI')
+    .setDescription('True mate AI API description')
+    .setVersion('1.0')
+    .addTag('TRUE MATE AI')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     logger.log(`Application listening on port ${port} or http://localhost:${port}`);
